ad8e514a7c43f8eb13b8336cd7e16a678573e436
scripture <125283797+greyliterature@users.noreply.github.com>
2026-04-22 17:59:06 -0400

    Create non-math helpers section, add PadString function

--- confint.r
+++ confint.r
@@ -1,1 +1,19 @@
+############################
+    #Non math helpers
+############################
 
+PadString = function(str, padlength, padstring){ #for binomial table formatting
+    #str = tostring(str)
+    stringtable = strsplit(str, "")[[1]]
+    for (i in 1:max(padlength, nchar(str))){
+        if (i > padlength){ 
+            stringtable[i] = ""
+        }
+        else if (i <= padlength && i > nchar(str)){
+            stringtable[i] = padstring
+        }
+    }
+    return (paste(stringtable, sep = "", collapse = ""))
+}
+#print(PadString("hello", 7, "d"))
+# output: "hellodd"
