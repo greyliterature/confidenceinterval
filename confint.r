df023ce193f6535d12beafd8ed002a83000fa18b
scripture <125283797+greyliterature@users.noreply.github.com>
2026-04-22 18:09:50 -0400

    Replace all comments with double "#", add bin() function

--- confint.r
+++ confint.r
@@ -1,9 +1,9 @@
 ############################
-    #Non math helpers
+    ##Non math helpers
 ############################
 
 PadString = function(str, padlength, padstring){ #for binomial table formatting
-    #str = tostring(str)
+    ##str = tostring(str)
     stringtable = strsplit(str, "")[[1]]
     for (i in 1:max(padlength, nchar(str))){
         if (i > padlength){ 
@@ -15,16 +15,29 @@ PadString = function(str, padlength, padstring){ #for binomial table formatting
     }
     return (paste(stringtable, sep = "", collapse = ""))
 }
-#print(PadString("hello", 7, "d"))
-# output: "hellodd"
+##print(PadString("hello", 7, "d"))
+## output: "hellodd"
 
 ############################
-    #Binomial math
+    ##Binomial math
 ############################
 NChooseK = function(n, k){
     return (factorial(n) / (factorial(k) * factorial(n - k)))
     ## n! / (k!(n - k)!)
 }
 
-#print(NChooseK(3, 2))
-# output: 3
+##print(NChooseK(3, 2))
+## output: 3
+##
+## PMF of binomial distribution: https://en.wikipedia.org/wiki/Binomial_distribution#Probability_mass_function
+## k = trials
+## n = sample amount
+## p = probability per trial
+bin = function(k, n, p){
+    FormulaChooseX = NChooseK(n, k)
+    return (FormulaChooseX * p ^ k * (1 - p) ^ (n - k))
+}
+
+##print(bin(4, 6, 0.3))
+## output: 0.059535
+## which matches https://en.wikipedia.org/wiki/Binomial_distribution#Example
